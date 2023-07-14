5d45fdd6437e251ff1227eeb657ab1a01e72db6e
MuhammadDawoodMaqbool <139550639+MuhammadDawoodMaqbool@users.noreply.github.com>
2023-07-14 18:38:24 +0500

    AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -57,13 +57,18 @@ export const AppReducer = (state, action) => {
                 ...state,
                 budget
             };
-        case 'SET_BUDGET':
-            action.type = "DONE";
-            state.budget = action.payload;
-
-            return {
-                ...state,
-            };
+        case'SET_BUDGET':
+        const newBudget = action.payload;
+        const expensesTotal = state.expenses.reduce((total, item) => {
+          return total + item.cost;
+        }, 0);
+        const remaining = newBudget - expensesTotal;
+      
+        return {
+          ...state,
+          budget: newBudget,
+          remaining: remaining,
+        }; 
         case 'CHG_CURRENCY':
             action.type = "DONE";
             state.currency = action.payload;
